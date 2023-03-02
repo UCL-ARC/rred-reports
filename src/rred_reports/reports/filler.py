2ba8c55018559bd7b977ccbc31f26c56a43d2c70
Harry Moss <h.moss@ucl.ac.uk>
2023-03-02 10:30:53 +0000

    Silence pylint

--- src/rred_reports/reports/filler.py
+++ src/rred_reports/reports/filler.py
@@ -6,6 +6,8 @@ from docx.table import Table
 
 
 class TemplateFillerException(Exception):
+    """Custom exception generator for the TemplateFiller class"""
+
     def __init__(self, message: str):
         super().__init__(message)
         self.message = message
@@ -15,6 +17,11 @@ class TemplateFillerException(Exception):
 
 
 class TemplateFiller:
+    """TemplateFiller class to modify tables in .docx files.
+
+    Provides save/loading, table modification and input verification.
+    """
+
     def __init__(self, template_path: Path):
         self.doc = Document(template_path)
         self.tables = self.doc.tables
@@ -29,8 +36,7 @@ class TemplateFiller:
         total_rows = len(table.rows) - 1
         for row_number in range(total_rows, 0, -1):
             row = table.rows[row_number]
-            row._element.getparent().remove(row._element)
-        return
+            row._element.getparent().remove(row._element)  # pylint: disable=protected-access
 
     def view_header(self, table: Table) -> list[Table.row_cells]:
         """View header text
@@ -44,7 +50,7 @@ class TemplateFiller:
         """
         return table.row_cells(0)
 
-    def populate_table(self, table: Table, df: pd.DataFrame):
+    def populate_table(self, table: Table, data: pd.DataFrame):
         """Populate a table with the contents of a pandas dataframe
 
         Args:
@@ -52,14 +58,14 @@ class TemplateFiller:
             df (pd.DataFrame): Pandas dataframe of future table contents
         """
         self.__class__.remove_all_rows(table)
-        self._verify_new_rows(table, df)
+        self._verify_new_rows(table, data)
 
-        for i in range(df.shape[0]):
+        for i in range(data.shape[0]):
             table.add_row()
-            for j in range(df.shape[-1]):
-                table.cell(i + 1, j).text = str(df.values[i, j])
+            for j in range(data.shape[-1]):
+                table.cell(i + 1, j).text = str(data.values[i, j])
 
-    def _verify_new_rows(self, table: Table, df: pd.DataFrame):
+    def _verify_new_rows(self, table: Table, data: pd.DataFrame):
         """Verify dimension of new rows to be added to existing table
         Pandas dataframe width should be the columnar dimension of the table
         i.e. the number of columns should match in both
@@ -71,8 +77,8 @@ class TemplateFiller:
         Raises:
             TemplateFillerException: DataFrame dimensions do not match table
         """
-        message = f"Pandas dataframe with shape {df.shape}. Table has {table.columns} columns - dimension mismatch."
-        if not df.shape[1] == len(table.columns):
+        message = f"Pandas dataframe with shape {data.shape}. Table has {table.columns} columns - dimension mismatch."
+        if not data.shape[1] == len(table.columns):
             raise TemplateFillerException(message)
 
     def save_document(self, output_path: Path):
