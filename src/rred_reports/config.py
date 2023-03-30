bac4b9c762f279ecf3e4e370124a0a755a1e12c8
Harry Moss <h.moss@ucl.ac.uk>
2023-03-30 21:31:38 +0100

    Add dynaconf config files

--- src/rred_reports/config.py
+++ src/rred_reports/config.py
@@ -1,8 +1,9 @@
-from dynaconf import Dynaconf
+from dynaconf import Dynaconf, Validator
 
 settings = Dynaconf(
     envvar_prefix="DYNACONF",
     settings_files=["settings.toml", ".secrets.toml"],
+    validators=[Validator("smtp_port", gte=5000, lte=9000), Validator("smtp_host", must_exist=True)],
 )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
