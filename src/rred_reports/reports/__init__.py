5bf1df792dedec6184eb79c9afbf215acaf504d7
Harry Moss <h.moss@ucl.ac.uk>
2023-03-31 11:00:20 +0100

    Add some basic auth components

--- src/rred_reports/reports/__init__.py
+++ src/rred_reports/reports/__init__.py
@@ -1,6 +1,9 @@
 """Filling report templates tables with RRED data"""
 from dataclasses import dataclass
 
+from dynaconf import Dynaconf
+from dynaconf.base import Settings
+
 
 @dataclass
 class ReportEmailConfig:
@@ -10,3 +13,17 @@ class ReportEmailConfig:
     recipients: str
     smtp_host: str
     smtp_port: int
+
+
+def get_settings() -> Settings:
+    """Retrieve Dynaconf settings
+    Looks at environment variables, settings.toml,
+    and finally .secrets.toml
+
+    Environment variables should be prefixed with
+    the envvar_prefix variable set in config.py
+
+    Returns:
+        Settings: Dynaconf settings
+    """
+    return Dynaconf()
--- src/rred_reports/reports/auth.py
+++ src/rred_reports/reports/auth.py
@@ -1,1 +1,32 @@
 """Mail server authentication"""
+from dataclasses import dataclass
+
+from dynaconf.base import Settings
+from exchangelib import (
+    DELEGATE,
+    Account,
+    Configuration,
+    OAuth2LegacyCredentials,
+)
+
+from rred_reports.reports import get_settings
+
+
+@dataclass
+class RREDAuthenticator:
+    settings: Settings = get_settings()
+
+    def get_credentials(self) -> OAuth2LegacyCredentials:
+        return OAuth2LegacyCredentials(
+            client_id=self.settings.client_id,
+            client_secret=self.settings.client_secret,
+            tenant_id=self.settings.tenant_id,
+            username=self.settings.username,
+            password=self.settings.auth_pass,
+        )
+
+    def get_config(self) -> Configuration:
+        return Configuration(server=self.settings.server, credentials=self.get_credentials())
+
+    def get_account(self) -> Account:
+        return Account(primary_smtp_address=self.settings.username, config=self.get_config(), access_type=DELEGATE)
