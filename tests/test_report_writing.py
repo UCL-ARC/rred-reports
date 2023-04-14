0f7dd234fb32969021f3904d66470a408128b548
Stef Piatek <s.piatek@ucl.ac.uk>
2023-04-14 11:33:21 +0100

    Ensure file is written

--- tests/test_report_writing.py
+++ tests/test_report_writing.py
@@ -2,11 +2,11 @@ from rred_reports.masterfile import join_masterfile_dfs, parse_masterfile
 from rred_reports.reports.tables import populate_school_tables, school_filter
 
 
-def test_school_tables(data_path, templates_dir, temp_out_dir):
+def test_school_tables_filled(data_path, templates_dir, temp_out_dir):
     """
     Given a masterfile with data from school RRS2030250, filtered to that school
     When the template for the school is populated using that data
-    Then a file should be written with the tables filled in
+    Then a file should be written with the tables filled in and a file should exist in the output path
     """
     nested_data = parse_masterfile(data_path / "example_masterfile.xlsx")
     testing_df = join_masterfile_dfs(nested_data)
@@ -16,3 +16,4 @@ def test_school_tables(data_path, templates_dir, temp_out_dir):
     populated_template = populate_school_tables(school_data, templates_dir / "2021/2021-22_template.docx", "RRS2030250", output_doc)
 
     assert populated_template.verify_tables_filled()
+    assert output_doc.exists()
