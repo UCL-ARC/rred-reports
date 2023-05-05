882be6eb1bf1027142ef745aee71df8c06c4fbcb
Stef Piatek <s.piatek@ucl.ac.uk>
2023-05-05 17:28:01 +0100

    Add interface for wide to long

---
+++ tests/test_redcap_interface.py
@@ -0,0 +1,32 @@
+import tomli_w
+
+from rred_reports.redcap.interface import extract
+
+
+def test_cli_writes_file(temp_out_dir):
+    """
+    Given a config file pointing to valid test data
+    When the extract CLI command is run, with an output to a temporary directory
+    Then a masterfile should be written to the output directory and no errors encountered
+    """
+    data_path = "tests/data"
+    test_config = {
+        "2021": {
+            "dispatch_list": f"{data_path}/dispatch_list.xlsx",
+            "current_year": {
+                "coded_data_file": f"{data_path}/redcap/extract.csv",
+                "label_data_file": f"{data_path}/redcap/extract_labels.csv",
+            },
+            "previous_year": {
+                "coded_data_file": f"{data_path}/redcap/extract.csv",
+                "label_data_file": f"{data_path}/redcap/extract_labels.csv",
+            },
+        }
+    }
+    config_path = temp_out_dir / "config.toml"
+    with config_path.open("wb") as handle:
+        tomli_w.dump(test_config, handle)
+    extract(2021, config_file=config_path, output_dir=temp_out_dir)
+
+    expected_file = temp_out_dir / "masterfile_2021-22.csv"
+    assert expected_file.exists()
