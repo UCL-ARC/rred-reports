32d416fd5ffedf08f24f214cb0a38e3d0afb083e
Katie Buntic <katiebuntic@eduroam-int-dhcp-97-104-235.ucl.ac.uk>
2023-05-09 14:36:47 +0100

    changing tests as masterfile has 2 more rows

--- tests/test_masterfile.py
+++ tests/test_masterfile.py
@@ -3,7 +3,7 @@ from rred_reports.masterfile import join_masterfile_dfs, parse_masterfile
 
 def test_masterfile_read(data_path):
     """
-    Given that a masterfile exists with 33 pupils, 10 teachers across 10 schools
+    Given that a masterfile exists with 35 pupils, 10 teachers across 10 schools
     When the masterfile is parsed and then joined
     The nested data should give the expected number of pupils, teachers and schools
     """
@@ -12,7 +12,7 @@ def test_masterfile_read(data_path):
 
     joined_data = join_masterfile_dfs(nested_data)
 
-    assert nested_data["pupils"].shape == (33, 64)
+    assert nested_data["pupils"].shape == (35, 64)
     assert nested_data["teachers"].shape == (10, 3)
     assert nested_data["schools"].shape == (10, 4)
-    assert joined_data.shape == (33, 69)  # should be the same number of students as in the pupils df
+    assert joined_data.shape == (35, 69)  # should be the same number of students as in the pupils df
